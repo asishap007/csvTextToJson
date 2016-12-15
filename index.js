a9ebc026cc01439a6deadfa2c4fcc63e272f62c5
asish_ap <asish.ap@accedo.tv>
2016-12-15 09:11:39 +0530

    convert number to string

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 /**
- * Created by ashishap on 14/12/16.
+ * Created by asishap on 14/12/16.
  */
+
+/**
+ * Adds commas to a number
+ * @param {number} number
+ * @param {string} locale
+ * @return {string}
+ */
+module.exports = function(number, locale) {
+    return number.toLocaleString(locale);
+};
